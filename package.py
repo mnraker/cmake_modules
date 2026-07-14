6e4c94b4d1376962f631284378ea859e66199e1b
Jon Lanz <1052296+jlanz@users.noreply.github.com>
2026-07-13 17:38:06 -0700

    Add support for evaluation of additional input types (#5)
    MOONRAY-6063 (#45)
    
    Signed-off-by: Jon Lanz <jon.lanz@dreamworks.com>
    Co-authored-by: Ramsey1, Paul (NBCUniversal) <Paul.Ramsey@dreamworks.com>

--- package.py
+++ package.py
@@ -11,7 +11,7 @@ def version():
     Increment the build in the version.
     """
     from json import load
-    _version = '1.1'
+    _version = '2.0'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
